d05499576acfe1506870418c95a83e82b2ced3d5
Guhan R <guhan5166@gmail.com>
2026-01-11 23:24:43 +0530

    Update WiFi credentials in wifi_led_control.ino

--- wifi_led_control.ino
+++ wifi_led_control.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "SSID Name";
-const char* password = "Password of SSID";
+const char* ssid = "Enter Your ssid";
+const char* password = "Enter Your Password";
 
 ESP8266WebServer server(80);
 
